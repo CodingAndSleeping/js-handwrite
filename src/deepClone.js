07d94fa69f42fe6b469e37e37016d62664d71770
Lizhitao <1162300237@qq.com>
2022-07-18 14:10:10 +0800

    深拷贝

--- src/deepClone.js
+++ src/deepClone.js
@@ -1,21 +1,28 @@
 function deepClone(obj) {
+    // 判断传入的是否是引用类型或者null，如果不是引用类型或者是null，则直接返回
     if (typeof obj !== "object" || obj == null) {
         return obj;
     }
-
+    // 定义一个新对象
     let newObj;
+    // 判断传进来的是数组还是对象
     if (obj instanceof Array) {
+        // 是数组就给newObj赋值一个空数组
         newObj = [];
     } else {
+        // 是对象就给newObj赋值一个空对象
         newObj = {};
     }
+    // 循环遍历对象上的key
     for (let key in obj) {
+        // 判断是否是对象本身上的key，对象原型身上的key不进行拷贝
         if (obj.hasOwnProperty(key)) {
+            // 递归调用deepClone
             newObj[key] = deepClone(obj[key]);
         }
 
     }
-
+    // 返回这个新对象
     return newObj;
 }
 
